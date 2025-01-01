f2156a13c01e7958061c94fa85ab2a39592f093a
Zachary Delcoco <zdelcoco@dmsi.com>
2025-01-01 10:56:27 -0600

    feat: add logout functionality and enhance tab bar icons in layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,77 @@
 import { Tabs } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
+import { useAppDispatch } from '@/hooks/redux';
+import { logout } from '@/store/authSlice';
+
+import Colors from '@/constants/Colors';
 
 export default function TabsLayout() {
+  const dispatch = useAppDispatch();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+  };
+
+  function ms(size: number): number {
+    const scale = 1.2; // Example scale factor
+    return Math.round(size * scale);
+  }
+
   return (
-    <Tabs>
-      <Tabs.Screen name="cards" options={{ title: 'Cards' }} />
-      <Tabs.Screen name="userProfile" options={{ title: 'Profile' }} />
-      <Tabs.Screen name="groups" options={{ title: 'Groups' }} />
+    <Tabs
+      screenOptions={{
+        headerRight: () => (
+          <FontAwesome
+            name='sign-out'
+            size={ms(24)}
+            color={Colors.light.tabIconDefault}
+            onPress={logoutHandler}
+            style={{ marginRight: 16 }}
+          />
+        ),
+        tabBarInactiveTintColor: Colors.light.tabIconDefault,
+        tabBarActiveTintColor: Colors.light.tabIconSelected,
+      }}
+    >
+      <Tabs.Screen
+        name='cards'
+        options={{
+          title: 'Cards',
+          tabBarIcon: ({ focused, color, size}) => (
+            <FontAwesome
+              name='vcard'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='userProfile'
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ focused, color, size}) => (
+            <FontAwesome
+              name='user'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='groups'
+        options={{
+          title: 'Groups',
+          tabBarIcon: ({ focused, color, size}) => (
+            <FontAwesome
+              name='users'
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
