d7512c0b27b0ed500b81c9094ff82e85f1fb602d
Zachary Delcoco <zdelcoco@dmsi.com>
2026-01-02 10:35:18 -0600

    feat: Change autoCapitalize to 'none' in PrayerSubjectPicker for improved text input handling

--- components/PrayerCards/PrayerSubjectPicker.tsx
+++ components/PrayerCards/PrayerSubjectPicker.tsx
@@ -295,7 +295,7 @@ export default function PrayerSubjectPicker({
             onBlur={() => {
               setTimeout(() => setShowAutocomplete(false), 200);
             }}
-            autoCapitalize="words"
+            autoCapitalize="none"
             autoCorrect={false}
             editable={!disabled}
           />
